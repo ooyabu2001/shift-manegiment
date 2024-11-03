1839b30b53fdcc447e6ee330ae396fc9a93d3f3c
EC2 Default User <ec2-user@ip-172-31-1-74.ap-northeast-1.compute.internal>
2024-11-03 09:42:12 +0000

    レイアウト調整

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,15 +4,14 @@ class Public::FavoritesController < ApplicationController
     message= Message.find(params[:message_id])
     favorite = current_user.favorites.new(message_id: message.id)
     favorite.save
-    redirect_to　group_messages_pathh(message.group, message)
+    redirect_to request.referer
   end
 
   def destroy
    message= Message.find(params[:message_id])
 favorite = current_user.favorites.find_by(message_id: message.id)
    favorite.destroy
-   redirect_to group_messages_path(message.group, message)
-
+   redirect_to request.referer#遷移元にredirectする
   end
 
 end
